f1a26020d7ed4b17d59e6243a98e32566f465155
unknown <ruchellguilherme100@gmail.com>
2023-11-14 15:35:54 -0300

    resolvendo problema de versão do swagger

--- src/main/java/com/schedule/vote/config/Info.java
+++
@@ -1,4 +0,0 @@
-package com.schedule.vote.config;
-
-public class Info {
-}
--- src/main/java/com/schedule/vote/config/SwaggerConfig.java
+++ src/main/java/com/schedule/vote/config/SwaggerConfig.java
@@ -1,17 +1,20 @@
 package com.schedule.vote.config;
 
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
-    public OpenAPI customOpenAPI(){
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .components(new Components())
-                .info(new Info().title("Swagger")
-                        .description("Swagger description"));
+            .components(new Components())
+            .info(new Info().title("Swagger")
+                .description("Swagger Description"));
     }
 }
