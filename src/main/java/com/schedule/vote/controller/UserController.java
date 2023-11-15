f9aab91b807bf24261ba98bbde27ecf6eed5fd37
GabbyVenturini <gabbyventurini@gmail.com>
2023-11-14 22:09:36 -0300

    implementanco teste de buscar usuario

--- src/main/java/com/schedule/vote/controller/UserController.java
+++ src/main/java/com/schedule/vote/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
 @RestController
 @RequestMapping("/user")
 public class UserController {
---
+++ src/test/java/com/schedule/vote/service/UserServiceTest.java
@@ -0,0 +1,35 @@
+package com.schedule.vote.service;
+
+import com.schedule.vote.model.User;
+import com.schedule.vote.repository.UserRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.mockito.BDDMockito.given;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+    @Mock
+    private UserRepository userRepository;
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    public void shouldReturnUser(){
+        var user = new User();
+        user.setId(1L);
+        user.setName("Gabby");
+
+        given(userRepository.findById(1L)).willReturn(Optional.of(user));
+
+        var result = userService.getUser(user.getId());
+        then(result.getId()).isEqualTo(1L);
+        then(result.getName()).isEqualTo("Gabby");
+    }
+}
